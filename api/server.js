f448d856b827b392ec6ad19f1516d03fc69ef915
ulvi05 <aghazadeulvi@gmail.com>
2025-10-28 16:25:08 +0400

    changes added

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
-
+const cors = require("cors");
 const server = jsonServer.create();
 
 const fs = require("fs");
@@ -10,23 +10,40 @@ const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
 
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
-
 const middlewares = jsonServer.defaults();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://movie-mania-silk.vercel.app",
+];
+
+server.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 server.use(middlewares);
-// Add this before server.use(router)
+
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
     "/blog/:resource/:id/show": "/:resource/:id",
   })
 );
+
 server.use(router);
+
 server.listen(3000, () => {
-  console.log("JSON Server is running");
+  console.log("✅ JSON Server is running on port 3000");
 });
 
-// Export the Server API
 module.exports = server;
